55196a5eaacb20bf4893b9113a64e892d979fe33
1 <1@qq.com>
2022-05-01 01:20:41 +0800

    test

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, LiaRen'
 author = 'LiaRen'
 
 # The full version, including alpha/beta/rc tags
-release = '-0.1'
+release = '0.1'
 
 
 # -- General configuration ---------------------------------------------------
